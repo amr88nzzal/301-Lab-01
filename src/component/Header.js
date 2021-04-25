63440de17b52c183b454bfc43a2c90940a45c8c5
amr88nzzal <amr.nzzal@gmail.com>
2021-04-25 23:12:05 +0300

    update

---
+++ src/component/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Header extends React.Component{
+    render(){
+        return(
+            <h1>Welcome To Horned Animals </h1>
+        )
+    }
+}
+export default Header
