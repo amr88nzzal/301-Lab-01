5ab1895c47c7e7e1b9113a9de08b198cc89390f2
amr88nzzal <amr.nzzal@gmail.com>
2021-04-27 04:40:26 +0300

    lab2

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 class Footer extends React.Component{
     render(){
         return(
+            <div>
+            <footer>
             <p>&copy; By  Amr Nzzal </p>
+            </footer>
+            </div>
         )
     }
 }
--- src/component/HornedBeast.js
+++ src/component/HornedBeast.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
 
 class HornedBeast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            like: 0
+        }
+    }
+    addLike = () => {
+        this.setState({ like: this.state.like + 1 })
+            ;
+    }
     render() {
         return (
             <div >
-                <h2>
-                {this.props.title}
-                </h2>
-                <img src = {this.props.imageUrl} alt = {`${this.props.description} with ${this.props.horns} `} title={this.props.title} />
-                <p>
-                {this.props.description}
-                </p>
+                <Card style={{ width: '' }}>
+                    <Card.Img  variant="top" src={this.props.imageUrl} />
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                        {this.props.description}
+                        </Card.Text>
+                        <Card.Text>
+                        {this.props.horns}
+                        </Card.Text>   
+                        <p>❤️ ({this.state.like})</p>
+                        <Button onClick={this.addLike}  variant="primary">Like 👍</Button>
+                    </Card.Body>
+                </Card>
             </div>
         )
     };
